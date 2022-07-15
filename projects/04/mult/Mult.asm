261ad375cb9f4b4e71e7a280fc25c57985fdbb85
Michael Willett <mikerwillett@gmail.com>
2022-07-14 20:55:34 -0500

    wrote out Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,46 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// ------------ pseudocode:
+// loop:
+//    if r1 == 0:
+//        exit
+//    r2 = r2 + r0
+//    r1--
+//    JMP loop
+
+
+// NOTES:
+// D: data register
+// A: address register
+// M: the selected RAM register
+
+
+// initialize R2 to 0
+@R2
+M = 0
+
+(LOOP)
+    // if R1 == 0: exit
+    @R1
+    D=M  // set the data register to the value of R1
+    @END
+    D; JEQ  // by using D, if R1 == 0: exit
+
+    @R1
+    M = M - 1  // decrement the value of R1
+
+    @R0
+    D = M  // we add R0 to itself exactly R1 times, so set the data register to the value of R0
+    @R2    // add R0 to R2
+    M = M + D
+
+    @LOOP
+    0; JMP
+
+
+(END)
+    @END
+    0; JMP  // infinite loop
+
